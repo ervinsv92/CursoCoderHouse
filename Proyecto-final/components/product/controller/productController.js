68260b1282fe37a872128c8d0ed8c272eabec55d
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-12-14 13:08:24 -0600

    avance proyecto final - parte 1

--- Proyecto-final/components/product/controller/productController.js
+++ Proyecto-final/components/product/controller/productController.js
@@ -3,8 +3,21 @@ const ProductService = require("../services/productService");
 const productService = new ProductService();
 
 class ProductController{
-    get(req, res, next){
+    async get(req, res, next){
         //aqui dentro de llama los servicios, y se responde al cliente
+        try {
+            const {id} = req.params;
+
+            if(id){
+                let product = await productService.getById(id);
+                res.json(product)
+            }else{
+                let products = await productService.getAll();
+                res.json(products)
+            }
+        } catch (error) {
+            res.status(400).json(error)
+        }
     }
 
     async save(req, res, next){
--- Proyecto-final/components/product/services/productService.js
+++ Proyecto-final/components/product/services/productService.js
@@ -3,9 +3,7 @@ const config = require("../../../config");
 const FileHelper = require("../../../utils/file/FileHelper");
 
 class ProductService{
-
     constructor(){
-        console.log("products", config.config.fileProducts)
         this.fileHelper = new FileHelper(config.config.fileProducts);
     }
 
@@ -15,6 +13,39 @@ class ProductService{
         list.push(product)
         await this.fileHelper.saveFile(list);
         return product;
+    }
+
+    async getAll(){
+        const list = await this.fileHelper.getAll() || [];
+        return list;
+    }
+
+    async getById(id){
+        const list = await this.fileHelper.getAll() || [];
+        const product = list.find(x=>x.id == id) || null;
+
+        if(!product){
+            throw new Error(`El producto con el id: ${id}, no se encuentra.`);
+        }
+
+        return product;
+    }
+
+    async update(product){
+        const list = await this.fileHelper.getAll() || [];
+        const idx = list.findIndex(x=>x.id == product.id);
+        list[idx] = product;
+        await this.fileHelper.saveFile(list);
+        return product;
+    }
+
+    async delete(id){
+        let list = await this.fileHelper.getAll() || [];
+        const idx = list.findIndex(x=>x.id == id);
+        const eliminado = list[idx];
+        list = list.filter(x => x.id != id);
+        await this.fileHelper.saveFile(list);
+        return eliminado;
     }
 }
 
