497003a7910a512abfff5c48f65f4ebf73016310
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-12-14 16:28:01 -0600

    mejoras

--- Proyecto-final/components/product/services/productService.js
+++ Proyecto-final/components/product/services/productService.js
@@ -10,6 +10,7 @@ class ProductService{
     async save(product){
         const list = await this.fileHelper.getAll() || [];
         product.id = uuid();
+        product.timestamp = Date.now();
         list.push(product)
         await this.fileHelper.saveFile(list);
         return product;
@@ -23,8 +24,8 @@ class ProductService{
     async getById(id){
         const list = await this.fileHelper.getAll() || [];
         const product = list.find(x=>x.id == id) || null;
-
-        if(!product){
+        console.log("pro: ", product)
+        if(product == null){
             throw new Error(`El producto con el id: ${id}, no se encuentra.`);
         }
 
@@ -34,6 +35,11 @@ class ProductService{
     async update(product){
         const list = await this.fileHelper.getAll() || [];
         const idx = list.findIndex(x=>x.id == product.id);
+
+        if(idx == -1){
+            throw new Error(`El producto con el id: ${product.id} no se encuentra.`);
+        }
+
         list[idx] = product;
         await this.fileHelper.saveFile(list);
         return product;
@@ -42,6 +48,11 @@ class ProductService{
     async delete(id){
         let list = await this.fileHelper.getAll() || [];
         const idx = list.findIndex(x=>x.id == id);
+
+        if(idx == -1){
+            throw new Error(`El producto con el id: ${id} no se encuentra.`);
+        }
+
         const eliminado = list[idx];
         list = list.filter(x => x.id != id);
         await this.fileHelper.saveFile(list);
