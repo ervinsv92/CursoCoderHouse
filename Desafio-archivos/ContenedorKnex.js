066b829fc03f69af8df9df824a889bfcdf2e94f2
Ervin <ervinsv92@gmail.com>
2021-12-21 21:49:13 -0600

    Desafio knex

---
+++ Desafio-archivos/ContenedorKnex.js
@@ -0,0 +1,75 @@
+class ContenedorKnex{
+    constructor(conexion, tabla){
+        this.conexion = conexion;
+        this.tabla = tabla;
+    }
+
+    async init(){
+        try {
+            await this.conexion.schema.createTable(this.tabla, (table)=>{
+                table.increments('id')
+                table.string('title')
+                table.integer('price')
+                table.string('thumbnail')
+            });
+        } catch (error) {
+            console.log("Crear tabla error: ", error);
+        }
+    }
+
+    async save(producto){
+        let id = -1;
+        try {
+            await this.conexion(this.tabla).insert({
+                title:producto.title,
+                price:producto.price,
+                thumbnail:producto.thumbnail
+            });
+            
+            let row = await this.conexion(this.tabla).select('id').orderBy('id', 'desc');
+            id = row[0].id;
+        } catch (error) {
+            console.log("Save error: ", error)
+        }
+
+        return id;
+    }
+
+    getById = async (id)=> {
+        let product = null;
+        try {
+            product = await this.conexion(this.tabla).where({id:id}).first();
+        } catch (error) {
+            console.log("getById: ", error);
+        }
+
+        return product;
+    };
+
+    async getAll(){
+        let products = [];
+        try {
+            products = await this.conexion(this.tabla);
+        } catch (error) {
+            console.error("getAll:", error)
+        }
+
+        return products;
+    }
+    async deleteById(id){
+       try {
+        await this.conexion(this.tabla).where({id:id}).del();
+       } catch (error) {
+        console.error("deleteById:", error)
+       }
+    }
+    async deleteAll(){
+        try {
+            await this.conexion(this.tabla).del();
+           } catch (error) {
+            console.error("deleteById:", error)
+           }
+    }
+}
+
+module.exports = ContenedorKnex;
--- Desafio-archivos/index.js
+++ Desafio-archivos/index.js
@@ -1,8 +1,46 @@
-const Contenedor = require('./Contenedor')
+/**
+ * Hay 3 tipos de almacenamiento
+ * 1- Archivos
+ * 2- Sqlite3
+ * 3- MariaDB
+ * 
+ * descomentar uno y comentar los otros para realizar las pruebas
+ */
+
+const Contenedor = require('./Contenedor');
+const ContenedorKnex = require('./ContenedorKnex');
 
 const prueba = async ()=>{
-    const cont = new Contenedor();
+    ////1- Archivos *****************************************
+    //const cont = new Contenedor();
+    //await cont.init();
+    //// Fin 1- Archivos ************************************
+
+    //2- Sqlite3 ***********************************************
+    let cnnSqlite3 = require('knex')({
+        client:'sqlite3',
+        connection:{filename:'./db/ecomerce.sqlite'}
+    });
+
+    const cont = new ContenedorKnex(cnnSqlite3, 'products');
     await cont.init();
+    // FIN 2- Sqlite2 ******************************************
+
+
+    // //3- MariaDb ***********************************************
+    // let cnnMariaDB = require('knex')({
+    //     client:'mysql',
+    //     connection:{
+    //         host:'127.0.0.1',
+    //         user:'root',
+    //         password:'1234',
+    //         database:'knex'
+    //     }
+    // });
+
+    // const cont = new ContenedorKnex(cnnMariaDB, 'products');
+    // await cont.init();
+    // // FIN 3- MariaDB ******************************************
 
     const idSave1 = await cont.save({
         title:'Guitarra',
@@ -25,12 +63,13 @@ const prueba = async ()=>{
     })
 
     console.log("getAll: ", await cont.getAll(), "\n");
+    //revisar en bd el id porque es autoincrement
     console.log("getById (2): ", await cont.getById(2), "\n");
+    //revisar en bd el id porque es autoincrement
     console.log("deleteById (3): ", await cont.deleteById(3), "\n");
     console.log("getAll: ", await cont.getAll(), "\n");
     console.log("deleteAll: ", await cont.deleteAll(), "\n");
-    console.log("getAll: ", await cont.getAll(), "\n");
-
+    
     //Inserta un elemento al archivo despues de borrarlos todos, para que haya algo en el txt la proxima vez que se ejecute la aplicación
     await cont.save({
         title:'Celular',
